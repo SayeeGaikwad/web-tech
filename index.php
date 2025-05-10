1d4d8b6171b5eade627144ab04aa99b0058719c9
SayeeGaikwad <sayeegaikwad518@gmail.com>
2025-05-10 17:24:02 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
 
-$fruits = array("Apple", "Banana", "Mango", "Orange");
-
-$fruits[] = "Pineapple";
-
-echo "List of Fruits:\n";
-foreach ($fruits as $fruit) 
-{
-    echo $fruit . "\n";
-}
-
-$prices = array(
-    "Apple" => 100,
-    "Mango" => 120,
-    "Orange" => 80
-);
-
-$prices["Pineapple"] = 150;
-
-echo "\nFruit Prices:\n";
-foreach ($prices as $fruit => $price) {
-    echo $fruit . ": ₹" . $price . "\n";
+$city = array("Kolhapur", "Mumbai", "Pune");
+foreach ($city as $x) {
+  echo "$x <br>";
 }
 ?>
